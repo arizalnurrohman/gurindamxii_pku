dc678ce680a6b3877ff5c4b4926bc937284f557b
Arizal Nur Rohman <lawlietsan1302@gmail.com>
2023-10-31 15:27:02 +0700

    FOLDER VENDOR

--- gurindam.sql
+++ gurindam.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 31, 2023 at 05:10 AM
+-- Generation Time: Oct 31, 2023 at 05:40 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
